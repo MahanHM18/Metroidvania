dd878f7280f3443e824d43c0373ad0a98c4d2891
mahan <91977844+MahanHM18@users.noreply.github.com>
2021-12-11 12:15:17 +0330

    Make the player movement

---
+++ Metroidvania/Assets/Scripts/Penitent/PenitentInput.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenitentInput : MonoBehaviour
+{
+    public bool LeftKey { get { return Input.GetKey(KeyCode.LeftArrow); } }
+    public bool RighttKey { get { return Input.GetKey(KeyCode.RightArrow); } }
+    
+    public bool Jump { get { return Input.GetKey(KeyCode.V);  } }
+
+}
---
+++ Metroidvania/Assets/Scripts/Penitent/PenitentMovement.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenitentMovement : MonoBehaviour
+{
+    [SerializeField] private float MoveSpeed, SoftTime, JumpForce;
+    [SerializeField] private LayerMask GroundLayer;
+    [HideInInspector] public float Speed;
+    private Rigidbody2D _rb;
+    private PenitentInput _input;
+    private float _stepTimer;
+    private BoxCollider2D _boxCollider2D;
+
+    private bool IsGrounded { get { return Physics2D.BoxCast(_boxCollider2D.bounds.center, _boxCollider2D.size, 0, Vector2.down, 0.1f, GroundLayer); } }
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _input = GetComponent<PenitentInput>();
+        _boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+    void Start()
+    {
+        Speed = MoveSpeed;
+    }
+
+
+    void Update()
+    {
+        PlayerMovement();
+        PlayerJump();
+    }
+
+    private void PlayerMovement()
+    {
+        if (_input.LeftKey && _input.RighttKey)
+        {
+            if (_stepTimer >= 0)
+                _stepTimer -= Time.deltaTime;
+            if (_stepTimer <= 0)
+                _rb.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX;
+            _rb.velocity = new Vector2(Mathf.Lerp(_rb.velocity.x, 0, Time.deltaTime * SoftTime), _rb.velocity.y);
+        }
+        else if (_input.RighttKey)
+        {
+            _stepTimer = 0.3f;
+            _rb.velocity = new Vector2(Speed, _rb.velocity.y);
+            _rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+            transform.localScale = new Vector2(1, 1);
+        }
+        else if (_input.LeftKey)
+        {
+            _stepTimer = 0.3f;
+            _rb.velocity = new Vector2(-Speed, _rb.velocity.y);
+            _rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+            transform.localScale = new Vector2(-1, 1);
+        }
+        else if (!_input.LeftKey && !_input.RighttKey)
+        {
+            if (_stepTimer >= 0)
+                _stepTimer -= Time.deltaTime;
+            if (_stepTimer <= 0)
+                _rb.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX;
+            _rb.velocity = new Vector2(Mathf.Lerp(_rb.velocity.x, 0, Time.deltaTime * SoftTime), _rb.velocity.y);
+        }
+    }
+    private void PlayerJump()
+    {
+        if (_input.Jump)
+        {
+            if (IsGrounded)
+            {
+                _rb.velocity = new Vector2(_rb.velocity.x, JumpForce);
+            }
+        }
+    }
+}
